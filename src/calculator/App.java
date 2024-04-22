86e5ffdb13bd4228cc75f7fc9dd8d146f55da89e
LYS <01034895017a@gmail.com>
2024-04-22 20:13:09 +0900

    update level 1-1 requirement
    level 1-1 요구 사항 추가

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -1,4 +1,18 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt(); // 정수형 변수를 선언, nextInt로 입력받음
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("num1, num2 = " + num1 + ", " + num2);
+    }
 }
