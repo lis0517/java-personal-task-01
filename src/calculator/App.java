a7ef3764fc5cf643d1c28c934857b58a4314b598
LYS <01034895017a@gmail.com>
2024-04-23 15:19:17 +0900

    update level 1-6 requirement
    연산 결과가 10개를 초과하는 경우 가장 먼저 저장된 결과를 삭제하고 새로운 연산 결과가 저장될 수 있도록 소스 코드를 수정.

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -3,20 +3,21 @@ package calculator;
 import java.util.Scanner;
 
 public class App {
+    private static final int MAX_HISTORY_SIZE = 10;
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         int idx = 0;
-        int[] numArray = new int[10];
+        int[] resultHistory = new int[MAX_HISTORY_SIZE];
 
         while(true)
         {
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt(); // 정수형 변수를 선언, nextInt로 입력받음
+            int firstNumber = sc.nextInt(); // 정수형 변수를 선언, nextInt로 입력받음
 
             System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+            int secondNumber = sc.nextInt();
 
             System.out.print("사칙연산 기호를 입력하세요 : "); // +, -, *, /
 
@@ -27,20 +28,20 @@ public class App {
             int result = 0;
             switch(operator){ // switch 문을 사용해 기호에 맞는 연산 실행
                 case '+':
-                    result = num1 + num2;
+                    result = firstNumber + secondNumber;
                     break;
                 case '-':
-                    result = num1 - num2;
+                    result = firstNumber - secondNumber;
                     break;
                 case '*':
-                    result = num1 * num2;
+                    result = firstNumber * secondNumber;
                     break;
                 case '/':
-                    if ( num2 == 0 ) { // 분모가 0인 경우 예외처리
+                    if ( secondNumber == 0 ) { // 분모가 0인 경우 예외처리
                         System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                         break;
                     }
-                    result = num1 / num2;
+                    result = firstNumber / secondNumber;
                     break;
                 default: // 정의되지 않은 기호의 경우 처리
                     System.out.println("정의되어있는 사칙 연산 기호가 아닙니다.");
@@ -48,8 +49,18 @@ public class App {
             }
             System.out.println("결과: " + result);
 
-            if ( idx < 10 ) // index가 10과 같거나 넘어가면 오류 발생
-                numArray[idx++] = result; // 비어있는 곳에 저장 후 다음 index로 증가
+
+            if ( idx >= MAX_HISTORY_SIZE ){ // MAX_HISTORY_SIZE 개를 초과하는 경우 가장 먼저 저장된 결과를 삭제하고 새로운 결과가 저장될 수 있도록 수정
+                for ( int i = 0; i < MAX_HISTORY_SIZE; ++i ){
+                    if ( i == MAX_HISTORY_SIZE - 1 )
+                        resultHistory[i] = result;
+                    else
+                        resultHistory[i] = resultHistory[i + 1];
+                }
+            }
+            else{
+                resultHistory[idx++] = result; // 비어있는 곳에 저장 후 다음 index로 증가
+            }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
 
